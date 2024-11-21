d766fdeb8e1364240488b78df1532f377faa9e4a
JUNGHO-GIT <junghomun00@gmail.com>
2024-11-22 04:20:26 +0900

    2024-11-22  4:20:26

--- gcloud.cjs
+++ gcloud.cjs
@@ -40,7 +40,7 @@ const modifyChangelog = () => {
 
     let newVersion = `\\[ ${versionArray.join('.')} \\]`;
     let newDateTime = `- ${currentDate} (${currentTime})`;
-    let newEntry = `\n\n## ${newVersion}\n\t${newDateTime}\n`;
+    let newEntry = `\n\n## ${newVersion}\n\t${newDateTime}`;
 
     newEntry = newEntry.replace(/([.]\s*[(])/g, ' (');
     newEntry = newEntry.replace(/([.]\s*)/g, '-');
