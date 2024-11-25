76e3c8326b3154163bdab864b3b451606a0fe9ec
JUNGHO-GIT <junghomun00@gmail.com>
2024-11-25 15:33:13 +0900

    2024-11-25 15:33:13

--- gcloud.cjs
+++ gcloud.cjs
@@ -143,7 +143,7 @@ const runRemoteScript = () => {
     const cmdGitReset = 'sudo git reset --hard origin/master';
     const cmdRmClient = 'sudo rm -rf client';
     const cmdCh = 'sudo chmod -R 755 /var/www/junghomun.com/JREACT/server';
-    const cmdStop = 'sudo pm2 stop JREACT && pm2 save --force';
+    const cmdStop = 'pm2 describe JREACT >/dev/null 2>&1 && sudo pm2 stop JREACT && pm2 save --force';
     const cmdNpm = 'sudo npm install';
     const cmdStart = 'sudo pm2 start ecosystem.config.cjs --env production && pm2 save --force';
 
