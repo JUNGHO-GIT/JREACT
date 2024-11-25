06b4fff6cda80e0ef7a13798052adb92c5e74bb5
JUNGHO-GIT <junghomun00@gmail.com>
2024-11-25 16:00:35 +0900

    2024-11-25 16:00:35

--- gcloud.cjs
+++ gcloud.cjs
@@ -143,13 +143,14 @@ const runRemoteScript = () => {
     const cmdGitReset = 'sudo git reset --hard origin/master';
     const cmdRmClient = 'sudo rm -rf client';
     const cmdCh = 'sudo chmod -R 755 /var/www/junghomun.com/JREACT/server';
-    const cmdStop = 'if pm2 describe JREACT >/dev/null 2>&1; then sudo pm2 stop JREACT && pm2 save; fi';
+    const cmdStop = 'if pm2 describe JREACT >/dev/null 2>&1; then sudo pm2 stop JREACT; fi';
     const cmdNpm = 'sudo npm install';
-    const cmdStart = 'sudo pm2 start ecosystem.config.cjs --env production && pm2 save';
+    const cmdStart = 'sudo pm2 start ecosystem.config.cjs --env production';
+    const cmdSave = 'sleep 5 && sudo pm2 save --force';
 
-    const winCommand = `powershell -Command "ssh -i ${keyPath} ${serviceId}@${ipAddr} \'${cmdCd} && ${cmdGitFetch} && ${cmdGitReset} && ${cmdRmClient} && ${cmdCh} && ${cmdStop} && ${cmdNpm} && ${cmdStart}\'"`;
+    const winCommand = `powershell -Command "ssh -i ${keyPath} ${serviceId}@${ipAddr} \'${cmdCd} && ${cmdGitFetch} && ${cmdGitReset} && ${cmdRmClient} && ${cmdCh} && ${cmdStop} && ${cmdNpm} && ${cmdStart} && ${cmdSave}\'"`;
 
-    const linuxCommand = `ssh -i ${keyPath} ${serviceId}@${ipAddr} \'${cmdCd} && ${cmdGitFetch} && ${cmdGitReset} && ${cmdRmClient} && ${cmdCh} && ${cmdStop} && ${cmdNpm} && ${cmdStart}\'`;
+    const linuxCommand = `ssh -i ${keyPath} ${serviceId}@${ipAddr} \'${cmdCd} && ${cmdGitFetch} && ${cmdGitReset} && ${cmdRmClient} && ${cmdCh} && ${cmdStop} && ${cmdNpm} && ${cmdStart} && ${cmdSave}\'`;
 
     const sshCommand = winOrLinux === "win" ? winCommand : linuxCommand;
     execSync(sshCommand, { stdio: 'inherit' });
