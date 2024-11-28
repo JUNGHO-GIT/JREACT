baf547c755216a4e1a425f223a47e0cfd4ce7680
JUNGHO-GIT <junghomun00@gmail.com>
2024-11-28 22:22:04 +0900

    2024-11-28 22:22:04

---
+++ client/src/schemas/Admin.tsx
@@ -0,0 +1,17 @@
+// Admin.tsx
+
+// -------------------------------------------------------------------------------------------------
+export const Admin = {
+
+  _id: "",
+
+  admin_date: "",
+  adminSection: [
+    {
+      admin_visit_ip: "",
+    }
+  ],
+
+  admin_regDt: null,
+  admin_updateDt: null,
+};
